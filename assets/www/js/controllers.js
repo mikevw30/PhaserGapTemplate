886d29fc1e6f2054509be643f47e0031d08ace48
Michael Van Wie <mikevw30@gmail.com>
2015-12-12 18:56:02 -0500

    this is going to break stuff

---
+++ assets/www/js/controllers.js
@@ -0,0 +1,45 @@
+var myApp = angular.module('myApp', [ 'ngRoute' ]);
+
+myApp.config(function($locationProvider, $routeProvider) {
+	$locationProvider.hashPrefix("!");
+	$locationProvider.html5Mode(false);
+	$routeProvider.when('/view1', {
+		controller : 'mainController',
+		templateUrl : 'frags/view1.html'
+	}).when('/view2', {
+		controller : 'mainController',
+		templateUrl : 'frags/view2.html'
+	}).otherwise({
+		redirectTo : '/view1'
+	});
+});
+
+var controllers = {};
+
+controllers.mainController = function($scope) {
+	$scope.customers = [ {
+		name : 'Kyle Two',
+		city : 'interlachen'
+	}, {
+		name : 'Andrew Three',
+		city : 'keystone'
+	}, {
+		name : 'Mike One',
+		city : 'gainesville'
+	} ];
+};
+
+controllers.mainController2 = function($scope) {
+	$scope.customers = [ {
+		name : 'Kyle Two',
+		city : 'interlachen'
+	}, {
+		name : 'Andrew Three',
+		city : 'keystone'
+	}, {
+		name : 'Mike One',
+		city : 'gainesville'
+	} ];
+};
+
+myApp.controller(controllers);
--- assets/www/js/controllers/mainController.js
+++
@@ -1,4 +0,0 @@
- var helloApp = angular.module("HelloApp", []);
-     helloApp.controller("HelloCtrl", [ '$scope', function($scope) {
-     $scope.name = "World";
-     } ]);
