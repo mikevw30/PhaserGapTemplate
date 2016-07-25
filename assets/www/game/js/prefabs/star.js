ec1adb059485d0b1507585fa8d134e534036ea1d
Michael Van Wie <mikevw30@gmail.com>
2016-07-24 23:56:07 -0400

    sdfsdf

--- assets/www/game/js/prefabs/star.js
+++ assets/www/game/js/prefabs/star.js
@@ -14,22 +14,22 @@ var Star = function(game, x, y, frame) {
   var d1 = y; 
   var t1 = d1/speed;
   
-  var d2 = (height-this.height);
+  var d2 = (height-this.height/2);
   var t2 = d2/speed;
 
-  var d3 = (height-this.height) - y;
+  var d3 = (height-this.height/2) - y;
   var t3 = d3/speed;
   
   var ran = game.rnd.integerInRange(1, 100);
   
   if(ran>50){
 	  game.add.tween(this).to({y:0},t1,Phaser.Easing.Linear.NONE)
-	  					  .to({y:height-this.height},t2,Phaser.Easing.Linear.NONE)
+	  					  .to({y:height-this.height/2},t2,Phaser.Easing.Linear.NONE)
 	  					  .to({y:y},t3,Phaser.Easing.Linear.NONE)
 	  					  .loop(true).start();
   }
   else{
-	  game.add.tween(this).to({y:height-this.height},t3,Phaser.Easing.Linear.NONE)
+	  game.add.tween(this).to({y:height-this.height/2},t3,Phaser.Easing.Linear.NONE)
 						  .to({y:0},t2,Phaser.Easing.Linear.NONE)
 						  .to({y:y},t1,Phaser.Easing.Linear.NONE)
 						  .loop(true).start();
--- assets/www/game/js/prefabs/starGroup.js
+++ assets/www/game/js/prefabs/starGroup.js
@@ -3,7 +3,7 @@
 var StarGroup = function(game, parent) {
 
   Phaser.Group.call(this, game, parent);
-  this.add(new Star(game, window.width, Math.ceil(Math.random()*10) * 60 + 10));   
+  this.add(new Star(game, window.width, game.rnd.integerInRange(0, window.height/(16*3)-1) *(16*3)-(16*3)/2));   
   this.setAll('body.velocity.x', -200);
 };
 
